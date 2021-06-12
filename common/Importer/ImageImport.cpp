c7896988dbe4e41f0ff7cd0e570cc87248fdf73e
Valdemar Lindberg <voldiekami@gmail.com>
2021-06-12 09:29:53 +0200

    Improved importer

--- common/Importer/ImageImport.cpp
+++ common/Importer/ImageImport.cpp
@@ -107,3 +107,47 @@ void *ImageImporter::loadTextureData(const char *cfilename, unsigned int *pwidth
 	FreeImage_CloseMemory(stream);
 	return pixels;
 }
+
+void ImageImporter::createImage(const char *filename, VkDevice device, VkCommandBuffer cmd,
+									 VkPhysicalDevice physicalDevice, VkImage &textureImage,
+									 VkDeviceMemory &textureImageMemory) {
+	unsigned int texWidth, texHeight, internal, type, format;
+	unsigned long pixelSize;
+	void *pixels = loadTextureData(filename, &texWidth, &texHeight, &format, &internal, &type, &pixelSize);
+
+	VkDeviceSize imageSize = texWidth * texHeight * 4;
+	VkPhysicalDeviceMemoryProperties memProperties;
+
+	vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+
+	if (!pixels) {
+		throw std::runtime_error("failed to load texture image!");
+	}
+
+	// VkBuffer stagingBuffer;
+	// VkDeviceMemory stagingBufferMemory;
+	// VKHelper::createBuffer(device, imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+	// 					   VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+	// 					   stagingBuffer, stagingBufferMemory);
+
+	// void *data;
+	// vkMapMemory(device, stagingBufferMemory, 0, imageSize, 0, &data);
+	// memcpy(data, pixels, static_cast<size_t>(imageSize));
+	// vkUnmapMemory(device, stagingBufferMemory);
+
+	// free(pixels);
+
+	// VKHelper::createImage(device, texWidth, texHeight, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL,
+	// 					  VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+	// 					  VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, memProperties, textureImage, textureImageMemory);
+
+	// VKHelper::transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED,
+	// 					  VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+	// // copyBufferToImage(stagingBuffer, textureImage, static_cast<uint32_t>(texWidth),
+	// // 				  static_cast<uint32_t>(texHeight));
+	// VKHelper::transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+	// 								VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+
+	// vkDestroyBuffer(device, stagingBuffer, nullptr);
+	// vkFreeMemory(device, stagingBufferMemory, nullptr);
+}
--- common/Importer/ImageImport.h
+++ common/Importer/ImageImport.h
@@ -1,7 +1,9 @@
 #ifndef _VK_SAMPLE_IMAGE_IMPORTER_H_
 #define _VK_SAMPLE_IMAGE_IMPORTER_H_ 1
+#include "../VKHelper.h"
+#include"../VKDevice.h"
 #include <stdio.h>
-#include<vulkan/vulkan.h>
+#include <vulkan/vulkan.h>
 
 /**
  * @brief
@@ -44,39 +46,39 @@ class ImageImporter {
 					   // encoding in byte 1, an 8-bit B component stored with sRGB nonlinear encoding in byte 2, and an
 					   // 8-bit A component in byte 3.
 		R8_UNorm,	   // A one-component, 8-bit unsigned normalized fvformatf that has a single 8-bit R component.
-		R8G8_UNorm, // A two-component, 16-bit unsigned normalized fvformatf that has an 8-bit R component stored with
-					// sRGB nonlinear encoding in byte 0, and an 8-bit G component stored with sRGB nonlinear encoding
-					// in byte 1.
-		R8G8B8_UNorm,	// A three-component, 24-bit unsigned normalized fvformatf that has an 8-bit R component in byte
-						// 0, an 8-bit G component in byte 1, and an 8-bit B component in byte 2.
+		R8G8_UNorm,	  // A two-component, 16-bit unsigned normalized fvformatf that has an 8-bit R component stored with
+					  // sRGB nonlinear encoding in byte 0, and an 8-bit G component stored with sRGB nonlinear encoding
+					  // in byte 1.
+		R8G8B8_UNorm, // A three-component, 24-bit unsigned normalized fvformatf that has an 8-bit R component in byte
+					  // 0, an 8-bit G component in byte 1, and an 8-bit B component in byte 2.
 		R8G8B8A8_UNorm, //	A four-component, 32-bit unsigned normalized fvformatf that has an 8-bit R component in byte
 						// 0, an 8-bit G component in byte 1, an 8-bit B component in byte 2, and an 8-bit A component
 						// in byte 3.
-		R8_SNorm,	//	A one-component, 8-bit signed normalized fvformatf that has a single 8-bit R component.
-		R8G8_SNorm, //	A two-component, 16-bit signed normalized fvformatf that has an 8-bit R component stored with
-					// sRGB nonlinear encoding in byte 0, and an 8-bit G component stored with sRGB nonlinear encoding
-					// in byte 1.
+		R8_SNorm,	  //	A one-component, 8-bit signed normalized fvformatf that has a single 8-bit R component.
+		R8G8_SNorm,	  //	A two-component, 16-bit signed normalized fvformatf that has an 8-bit R component stored with
+					  // sRGB nonlinear encoding in byte 0, and an 8-bit G component stored with sRGB nonlinear encoding
+					  // in byte 1.
 		R8G8B8_SNorm, //	A three-component, 24-bit signed normalized fvformatf that has an 8-bit R component in byte
 					  // 0, an 8-bit G component in byte 1, and an 8-bit B component in byte 2.
 		R8G8B8A8_SNorm, //	A four-component, 32-bit signed normalized fvformatf that has an 8-bit R component in byte
 						// 0, an 8-bit G component in byte 1, an 8-bit B component in byte 2, and an 8-bit A component
 						// in byte 3.
-		R8_UInt,   //	A one-component, 8-bit unsigned integer fvformatf that has a single 8-bit R component.
-		R8G8_UInt, //	A two-component, 16-bit unsigned integer fvformatf that has an 8-bit R component in byte 0, and
-				   // an 8-bit G component in byte 1.
+		R8_UInt,	 //	A one-component, 8-bit unsigned integer fvformatf that has a single 8-bit R component.
+		R8G8_UInt,	 //	A two-component, 16-bit unsigned integer fvformatf that has an 8-bit R component in byte 0, and
+					 // an 8-bit G component in byte 1.
 		R8G8B8_UInt, //	A three-component, 24-bit unsigned integer fvformatf that has an 8-bit R component in byte 0, an
 					 // 8-bit G component in byte 1, and an 8-bit B component in byte 2.
 		R8G8B8A8_UInt, //	A four-component, 32-bit unsigned integer fvformatf that has an 8-bit R component in byte 0,
 					   // an 8-bit G component in byte 1, an 8-bit B component in byte 2, and an 8-bit A component in
 					   // byte 3.
-		R8_SInt,   //	A one-component, 8-bit signed integer fvformatf that has a single 8-bit R component.
-		R8G8_SInt, //	A two-component, 16-bit signed integer fvformatf that has an 8-bit R component in byte 0, and an
-				   // 8-bit G component in byte 1.
+		R8_SInt,	   //	A one-component, 8-bit signed integer fvformatf that has a single 8-bit R component.
+		R8G8_SInt,	 //	A two-component, 16-bit signed integer fvformatf that has an 8-bit R component in byte 0, and an
+					 // 8-bit G component in byte 1.
 		R8G8B8_SInt, //	A three-component, 24-bit signed integer fvformatf that has an 8-bit R component in byte 0, an
 					 // 8-bit G component in byte 1, and an 8-bit B component in byte 2.
-		R8G8B8A8_SInt, //	A four-component, 32-bit signed integer fvformatf that has an 8-bit R component in byte 0,
-					   // an 8-bit G component in byte 1, an 8-bit B component in byte 2, and an 8-bit A component in
-					   // byte 3.
+		R8G8B8A8_SInt,	 //	A four-component, 32-bit signed integer fvformatf that has an 8-bit R component in byte 0,
+						 // an 8-bit G component in byte 1, an 8-bit B component in byte 2, and an 8-bit A component in
+						 // byte 3.
 		R16_UNorm,		 //	A one-component, 16-bit unsigned normalized fvformatf that has a single 16-bit R component.
 		R16G16_UNorm,	 //	A two-component, 32-bit unsigned normalized fvformatf that has a 16-bit R component in bytes
 						 // 0..1, and a 16-bit G component in bytes 2..3.
@@ -85,7 +87,7 @@ class ImageImporter {
 		R16G16B16A16_UNorm, //	A four-component, 64-bit unsigned normalized fvformatf that has a 16-bit R component in
 							// bytes 0..1, a 16-bit G component in bytes 2..3, a 16-bit B component in bytes 4..5, and a
 							// 16-bit A component in bytes 6..7.
-		R16_SNorm,		 //	A one-component, 16-bit signed normalized fvformatf that has a single 16-bit R component.
+		R16_SNorm,			//	A one-component, 16-bit signed normalized fvformatf that has a single 16-bit R component.
 		R16G16_SNorm,	 //	A two-component, 32-bit signed normalized fvformatf that has a 16-bit R component in bytes
 						 // 0..1, and a 16-bit G component in bytes 2..3.
 		R16G16B16_SNorm, //	A three-component, 48-bit signed normalized fvformatf that has a 16-bit R component in bytes
@@ -93,7 +95,7 @@ class ImageImporter {
 		R16G16B16A16_SNorm, //	A four-component, 64-bit signed normalized fvformatf that has a 16-bit R component in
 							// bytes 0..1, a 16-bit G component in bytes 2..3, a 16-bit B component in bytes 4..5, and a
 							// 16-bit A component in bytes 6..7.
-		R16_UInt,	 //	A one-component, 16-bit unsigned integer fvformatf that has a single 16-bit R component.
+		R16_UInt,			//	A one-component, 16-bit unsigned integer fvformatf that has a single 16-bit R component.
 		R16G16_UInt, //	A two-component, 32-bit unsigned integer fvformatf that has a 16-bit R component in bytes 0..1,
 					 // and a 16-bit G component in bytes 2..3.
 		R16G16B16_UInt, //	A three-component, 48-bit unsigned integer fvformatf that has a 16-bit R component in bytes
@@ -101,7 +103,7 @@ class ImageImporter {
 		R16G16B16A16_UInt, //	A four-component, 64-bit unsigned integer fvformatf that has a 16-bit R component in
 						   // bytes 0..1, a 16-bit G component in bytes 2..3, a 16-bit B component in bytes 4..5, and a
 						   // 16-bit A component in bytes 6..7.
-		R16_SInt,	 //	A one-component, 16-bit signed integer fvformatf that has a single 16-bit R component.
+		R16_SInt,		   //	A one-component, 16-bit signed integer fvformatf that has a single 16-bit R component.
 		R16G16_SInt, //	A two-component, 32-bit signed integer fvformatf that has a 16-bit R component in bytes 0..1,
 					 // and a 16-bit G component in bytes 2..3.
 		R16G16B16_SInt, //	A three-component, 48-bit signed integer fvformatf that has a 16-bit R component in bytes
@@ -109,7 +111,7 @@ class ImageImporter {
 		R16G16B16A16_SInt, //	A four-component, 64-bit signed integer fvformatf that has a 16-bit R component in bytes
 						   // 0..1, a 16-bit G component in bytes 2..3, a 16-bit B component in bytes 4..5, and a 16-bit
 						   // A component in bytes 6..7.
-		R32_UInt,	 //	A one-component, 32-bit unsigned integer fvformatf that has a single 32-bit R component.
+		R32_UInt,		   //	A one-component, 32-bit unsigned integer fvformatf that has a single 32-bit R component.
 		R32G32_UInt, //	A two-component, 64-bit unsigned integer fvformatf that has a 32-bit R component in bytes 0..3,
 					 // and a 32-bit G component in bytes 4..7.
 		R32G32B32_UInt, //	A three-component, 96-bit unsigned integer fvformatf that has a 32-bit R component in bytes
@@ -117,7 +119,7 @@ class ImageImporter {
 		R32G32B32A32_UInt, //	A four-component, 128-bit unsigned integer fvformatf that has a 32-bit R component in
 						   // bytes 0..3, a 32-bit G component in bytes 4..7, a 32-bit B component in bytes 8..11, and a
 						   // 32-bit A component in bytes 12..15.
-		R32_SInt,	 //	A one-component, 32-bit signed integer fvformatf that has a single 32-bit R component.
+		R32_SInt,		   //	A one-component, 32-bit signed integer fvformatf that has a single 32-bit R component.
 		R32G32_SInt, //	A two-component, 64-bit signed integer fvformatf that has a 32-bit R component in bytes 0..3,
 					 // and a 32-bit G component in bytes 4..7.
 		R32G32B32_SInt, //	A three-component, 96-bit signed integer fvformatf that has a 32-bit R component in bytes
@@ -148,13 +150,13 @@ class ImageImporter {
 					   // with sRGB nonlinear encoding in byte 0, an 8-bit G component stored with sRGB nonlinear
 					   // encoding in byte 1, an 8-bit R component stored with sRGB nonlinear encoding in byte 2, and an
 					   // 8-bit A component in byte 3.
-		B8G8R8_UNorm, //	A three-component, 24-bit unsigned normalized fvformatf that has an 8-bit B component in
-					  // byte 0, an 8-bit G component in byte 1, and an 8-bit R component in byte 2.
+		B8G8R8_UNorm,  //	A three-component, 24-bit unsigned normalized fvformatf that has an 8-bit B component in
+					   // byte 0, an 8-bit G component in byte 1, and an 8-bit R component in byte 2.
 		B8G8R8A8_UNorm, //	A four-component, 32-bit unsigned normalized fvformatf that has an 8-bit B component in byte
 						// 0, an 8-bit G component in byte 1, an 8-bit R component in byte 2, and an 8-bit A component
 						// in byte 3.
-		B8G8R8_SNorm, //	A three-component, 24-bit signed normalized fvformatf that has an 8-bit B component in byte
-					  // 0, an 8-bit G component in byte 1, and an 8-bit R component in byte 2.
+		B8G8R8_SNorm,	//	A three-component, 24-bit signed normalized fvformatf that has an 8-bit B component in byte
+						// 0, an 8-bit G component in byte 1, and an 8-bit R component in byte 2.
 		B8G8R8A8_SNorm, //	A four-component, 32-bit signed normalized fvformatf that has an 8-bit B component in byte
 						// 0, an 8-bit G component in byte 1, an 8-bit R component in byte 2, and an 8-bit A component
 						// in byte 3.
@@ -174,12 +176,12 @@ class ImageImporter {
 		B4G4R4A4_UNormPack16, //	A four-component, 16-bit packed unsigned normalized fvformatf that has a 4-bit B
 							  // component in bits 12..15, a 4-bit G component in bits 8..11, a 4-bit R component in
 							  // bits 4..7, and a 4-bit A component in bits 0..3.
-		R5G6B5_UNormPack16, //	A three-component, 16-bit packed unsigned normalized fvformatf that has a 5-bit R
-							// component in bits 11..15, a 6-bit G component in bits 5..10, and a 5-bit B component in
-							// bits 0..4.
-		B5G6R5_UNormPack16, //	A three-component, 16-bit packed unsigned normalized fvformatf that has a 5-bit B
-							// component in bits 11..15, a 6-bit G component in bits 5..10, and a 5-bit R component in
-							// bits 0..4.
+		R5G6B5_UNormPack16,	  //	A three-component, 16-bit packed unsigned normalized fvformatf that has a 5-bit R
+							  // component in bits 11..15, a 6-bit G component in bits 5..10, and a 5-bit B component in
+							  // bits 0..4.
+		B5G6R5_UNormPack16,	  //	A three-component, 16-bit packed unsigned normalized fvformatf that has a 5-bit B
+							  // component in bits 11..15, a 6-bit G component in bits 5..10, and a 5-bit R component in
+							  // bits 0..4.
 		R5G5B5A1_UNormPack16, //	A four-component, 16-bit packed unsigned normalized fvformatf that has a 5-bit R
 							  // component in bits 11..15, a 5-bit G component in bits 6..10, a 5-bit B component in
 							  // bits 1..5, and a 1-bit A component in bit 0.
@@ -192,25 +194,25 @@ class ImageImporter {
 		E5B9G9R9_UFloatPack32, //	A three-component, 32-bit packed unsigned floating-point fvformatf that has a 5-bit
 							   // shared exponent in bits 27..31, a 9-bit B component mantissa in bits 18..26, a 9-bit G
 							   // component mantissa in bits 9..17, and a 9-bit R component mantissa in bits 0..8.
-		B10G11R11_UFloatPack32, //	A three-component, 32-bit packed unsigned floating-point fvformatf that has a 10-bit
-								// B component in bits 22..31, an 11-bit G component in bits 11..21, an 11-bit R
-								// component in bits 0..10.
+		B10G11R11_UFloatPack32,	 //	A three-component, 32-bit packed unsigned floating-point fvformatf that has a 10-bit
+								 // B component in bits 22..31, an 11-bit G component in bits 11..21, an 11-bit R
+								 // component in bits 0..10.
 		A2B10G10R10_UNormPack32, //	A four-component, 32-bit packed unsigned normalized fvformatf that has a 2-bit A
 								 // component in bits 30..31, a 10-bit B component in bits 20..29, a 10-bit G component
 								 // in bits 10..19, and a 10-bit R component in bits 0..9.
-		A2B10G10R10_UIntPack32, //	A four-component, 32-bit packed unsigned integer fvformatf that has a 2-bit A
-								// component in bits 30..31, a 10-bit B component in bits 20..29, a 10-bit G component
-								// in bits 10..19, and a 10-bit R component in bits 0..9.
-		A2B10G10R10_SIntPack32, //	A four-component, 32-bit packed signed integer fvformatf that has a 2-bit A
-								// component in bits 30..31, a 10-bit B component in bits 20..29, a 10-bit G component
-								// in bits 10..19, and a 10-bit R component in bits 0..9.
+		A2B10G10R10_UIntPack32,	 //	A four-component, 32-bit packed unsigned integer fvformatf that has a 2-bit A
+								 // component in bits 30..31, a 10-bit B component in bits 20..29, a 10-bit G component
+								 // in bits 10..19, and a 10-bit R component in bits 0..9.
+		A2B10G10R10_SIntPack32,	 //	A four-component, 32-bit packed signed integer fvformatf that has a 2-bit A
+								 // component in bits 30..31, a 10-bit B component in bits 20..29, a 10-bit G component
+								 // in bits 10..19, and a 10-bit R component in bits 0..9.
 		A2R10G10B10_UNormPack32, //	A four-component, 32-bit packed unsigned normalized fvformatf that has a 2-bit A
 								 // component in bits 30..31, a 10-bit R component in bits 20..29, a 10-bit G component
 								 // in bits 10..19, and a 10-bit B component in bits 0..9.
-		A2R10G10B10_UIntPack32, //	A four-component, 32-bit packed unsigned integer fvformatf that has a 2-bit A
-								// component in bits 30..31, a 10-bit R component in bits 20..29, a 10-bit G component
-								// in bits 10..19, and a 10-bit B component in bits 0..9.
-		A2R10G10B10_SIntPack32, //	A four-component, 32-bit packed signed integer fvformatf that has a 2-bit A
+		A2R10G10B10_UIntPack32,	 //	A four-component, 32-bit packed unsigned integer fvformatf that has a 2-bit A
+								 // component in bits 30..31, a 10-bit R component in bits 20..29, a 10-bit G component
+								 // in bits 10..19, and a 10-bit B component in bits 0..9.
+		A2R10G10B10_SIntPack32,	 //	A four-component, 32-bit packed signed integer fvformatf that has a 2-bit A
 	};
 
 	/**
@@ -311,7 +313,10 @@ class ImageImporter {
 								 unsigned int *pformat, unsigned int *pinternalformat, unsigned int *ptype,
 								 unsigned long *pixelSize);
 
-	static void createImage(const char *filename, VkImage& image);
+	static void createImage(const char *filename, const VKDevice &device, VkImage &image);
+
+	static void createImage(const char *filename, VkDevice device, VkCommandBuffer cmd, VkPhysicalDevice physicalDevice,
+							VkImage &textureImage, VkDeviceMemory &textureImageMemory);
 };
 
 #endif
