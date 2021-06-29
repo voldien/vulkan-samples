0ca1533c7f0ff69ed800c19475a40c8dc330f950
voldien <voldiekami@gmail.com>
2021-06-29 11:14:15 +0200

    Improved benchmark, between CPU to GPU

--- Memory/MemoryTransfer.cpp
+++ Memory/MemoryTransfer.cpp
@@ -9,7 +9,7 @@ int main(int argc, const char **argv) {
 	const int nrTransferSamples = 100;
 	const int roundRobin = 3;
 
-	const std::array<size_t, 3> memorySizes = {1024, 1024 * 1024, 1024 * 1024 * 1024};
+	const std::array<VkDeviceSize, 3> memorySizes = {1024, 1024 * 1024, 1024 * 1024 * 128};
 
 	std::vector<int64_t> timeSample(nrTransferSamples);
 
@@ -23,11 +23,11 @@ int main(int argc, const char **argv) {
 	std::vector<VkDeviceMemory> gpu2gpu(roundRobin);
 	std::vector<VkDeviceMemory> gpu2cpu(roundRobin);
 
-	std::unordered_map<const char *, bool> required_device_extensions = {};
+	std::unordered_map<const char *, bool> required_device_extensions = {{"VK_KHR_performance_query", false}};
 	try {
 		std::shared_ptr<VulkanCore> core = std::make_shared<VulkanCore>();
 		std::vector<std::shared_ptr<PhysicalDevice>> phyDevices = core->createPhysicalDevices();
-		std::shared_ptr<VKDevice> device = std::make_shared<VKDevice>(phyDevices);
+		std::shared_ptr<VKDevice> device = std::make_shared<VKDevice>(phyDevices, required_device_extensions);
 
 		VkQueue transfer = device->getDefaultTransfer();
 
@@ -41,7 +41,7 @@ int main(int argc, const char **argv) {
 		queryInfo.queryCount = memorySizes.size() * roundRobin;
 		queryInfo.pipelineStatistics = 0;
 
-		vkCreateQueryPool(device->getHandle(), &queryInfo, nullptr, &queryPool);
+		// vkCreateQueryPool(device->getHandle(), &queryInfo, nullptr, &queryPool);
 
 		// this->getLogicalDevice()->get
 		const VkPhysicalDeviceMemoryProperties &memProp = device->getPhysicalDevices()[0]->getMemoryProperties();
@@ -51,13 +51,14 @@ int main(int argc, const char **argv) {
 		std::vector<VkCommandBuffer> cmds = device->allocateCommandBuffers(commandPool, VK_COMMAND_BUFFER_LEVEL_PRIMARY,
 																		   memorySizes.size() * roundRobin);
 
-		/*	*/
+		/*	Allocate all buffers.	*/
 		for (int i = 0; i < roundRobin; i++) {
 			VKHelper::createBuffer(device->getHandle(), memorySizes[i], memProp, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
 								   VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, stagingBuffer[i], staging[i]);
 
 			VKHelper::createBuffer(device->getHandle(), memorySizes[i], memProp, VK_BUFFER_USAGE_TRANSFER_DST_BIT,
 								   VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, cpu2gpuBuffer[i], cpu2gpu[i]);
+
 			VKHelper::createBuffer(device->getHandle(), memorySizes[i], memProp, VK_BUFFER_USAGE_TRANSFER_DST_BIT,
 								   VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, gpu2gpuBuffer[i], gpu2gpu[i]);
 			VKHelper::createBuffer(device->getHandle(), memorySizes[i], memProp, VK_BUFFER_USAGE_TRANSFER_DST_BIT,
@@ -69,32 +70,38 @@ int main(int argc, const char **argv) {
 		cmdBufBeginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 		cmdBufBeginInfo.flags = 0;
 
-		vkBeginCommandBuffer(cmds[0], &cmdBufBeginInfo);
-		vkCmdResetQueryPool(cmds[0], queryPool, 0, memorySizes.size() * roundRobin);
-
-		VkBufferCopy bufferCopy;
-		bufferCopy.dstOffset = 0;
-		bufferCopy.srcOffset = 0;
-		bufferCopy.size = bufferSize;
-
 		for (int i = 0; i < roundRobin; i++) {
-			vkCmdBeginQuery(cmds[0], queryPool, i, 0);
-			vkCmdWriteTimestamp(cmds[0], VK_PIPELINE_STAGE_HOST_BIT, queryPool, i);
+			VkBufferCopy bufferCopy;
+			bufferCopy.dstOffset = 0;
+			bufferCopy.srcOffset = 0;
+			bufferCopy.size = memorySizes[i];
+
+			vkBeginCommandBuffer(cmds[0], &cmdBufBeginInfo);
+			// vkCmdResetQueryPool(cmds[0], queryPool, 0, memorySizes.size() * roundRobin);
+			// vkCmdBeginQuery(cmds[0], queryPool, i, 0);
 			vkCmdCopyBuffer(cmds[0], stagingBuffer[i], cpu2gpuBuffer[i], 1, &bufferCopy);
-			vkCmdEndQuery(cmds[0], queryPool, i);
+			// vkCmdEndQuery(cmds[0], queryPool, i);
+			vkEndCommandBuffer(cmds[0]);
+			/*	*/
+			uint64_t timeStamp = SDL_GetPerformanceCounter();
+			device->submitCommands(transfer, {cmds[0]}, {}, {}, VK_NULL_HANDLE, {});
+			/*	Start time.	*/
+			VKS_VALIDATE(vkQueueWaitIdle(transfer));
+			/*	End Timer.	*/
+			uint64_t elapsedTime = SDL_GetPerformanceCounter() - timeStamp;
+			timeSample[i] = elapsedTime;
+			vkResetCommandBuffer(cmds[0], VK_COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT);
 		}
 
-		vkEndCommandBuffer(cmds[0]);
-		device->submitCommands(transfer, {cmds[0]});
-		VKS_VALIDATE(vkQueueWaitIdle(transfer));
-
-		VKS_VALIDATE(vkGetQueryPoolResults(device->getHandle(), queryPool, 0, roundRobin, timeSample.size() * sizeof(int64_t),
-										   timeSample.data(), sizeof(uint64_t), VK_QUERY_RESULT_64_BIT | VK_QUERY_RESULT_WAIT_BIT));
-										   printf("\n%ld, %ld, %ld\n", timeSample[0], timeSample[1], timeSample[1]);
+		std::cout << "CPU to GPU Memory Transfer Speed" << std::endl;
+		for (int i = 0; i < timeSample.size(); i++) {
+			std::cout << (float)timeSample[i] / (float)SDL_GetPerformanceFrequency() << std::endl;
+		}
 		/*	GPU->GPU	*/
+		std::cout << "GPU to GPU Memory Transfer Speed" << std::endl;
 
 		/*	GPU->CPU	*/
-
+		std::cout << "GPU to CPU Memory Transfer Speed" << std::endl;
 		/*	*/
 		for (unsigned int i = 0; i < memProp.memoryHeapCount; i++) {
 			if (memProp.memoryHeaps[i].flags & VK_MEMORY_HEAP_DEVICE_LOCAL_BIT) {
@@ -102,6 +109,9 @@ int main(int argc, const char **argv) {
 				// VK_BUFFER_USAGE_TRANSFER_DST_BIT, memProp.memoryHeaps[i].flags, memo )
 			}
 		}
+
+		vkFreeCommandBuffers(device->getHandle(), commandPool, cmds.size(), cmds.data());
+		vkDestroyCommandPool(device->getHandle(), commandPool, nullptr);
 
 		/*	Release buffers.	*/
 		for (int i = 0; i < roundRobin; i++) {
