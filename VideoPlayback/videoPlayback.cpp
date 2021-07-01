878dc236e83129e5bd79401496ed6d2cecf435aa
voldie <voldiekami@gmail.com>
2021-07-01 19:33:44 +0200

    Improvement

--- VideoPlayback/videoPlayback.cpp
+++ VideoPlayback/videoPlayback.cpp
@@ -50,6 +50,8 @@ class AVVideoPlaybackWindow : public VKWindow {
 	/*  */
 	int videoStream;
 	int audioStream;
+	int video_width;
+	int video_height;
 	/*  */
 	struct AVFrame *frame;
 	struct AVFrame *frameoutput;
@@ -66,7 +68,12 @@ class AVVideoPlaybackWindow : public VKWindow {
 		: VKWindow(core, device, -1, -1, -1, -1) {
 		loadVideo(path);
 	}
-	~AVVideoPlaybackWindow(void) {}
+	~AVVideoPlaybackWindow(void) {
+		avformat_close_input(&pformatCtx);
+		// av_packet_free(&pPacket);
+		av_frame_free(&frame);
+		avcodec_free_context(&pVideoCtx);
+	}
 
 	virtual void Release(void) override {}
 
@@ -74,30 +81,33 @@ class AVVideoPlaybackWindow : public VKWindow {
 		int result;
 
 		this->pformatCtx = avformat_alloc_context();
+		if (!pformatCtx) {
+			throw std::runtime_error("ERROR could not allocate memory for Format Context");
+		}
 		// Determine the input-format:
 		this->pformatCtx->iformat = av_find_input_format(path);
 
-		result = avformat_open_input(&this->pformatCtx, path, 0, 0);
+		result = avformat_open_input(&this->pformatCtx, path, nullptr, nullptr);
 		if (result != 0) {
 			char buf[AV_ERROR_MAX_STRING_SIZE];
 			av_strerror(result, buf, sizeof(buf));
 			throw std::runtime_error(fmt::format("Failed to open input : %s", buf));
 		}
 
-		if ((result = avformat_find_stream_info(this->pformatCtx, NULL)) < 0) {
+		if ((result = avformat_find_stream_info(this->pformatCtx, nullptr)) < 0) {
 			char buf[AV_ERROR_MAX_STRING_SIZE];
 			av_strerror(result, buf, sizeof(buf));
 			throw std::runtime_error(fmt::format("Failed to retrieve info from stream info : %s", buf));
 		}
-		struct AVStream *video_st = NULL;
-		struct AVStream *audio_st = NULL;
+		struct AVStream *video_st = nullptr;
+		struct AVStream *audio_st = nullptr;
 
 		/*	Get video codecs.	*/
 		for (int x = 0; x < this->pformatCtx->nb_streams; x++) {
 			AVStream *stream = this->pformatCtx->streams[x];
 
 			/*  */
-			if (stream->codecpar == NULL)
+			if (stream->codecpar == nullptr)
 				continue;
 
 			switch (stream->codecpar->codec_type) {
@@ -134,7 +144,7 @@ class AVVideoPlaybackWindow : public VKWindow {
 				throw std::runtime_error(fmt::format("Failed to set codec parameters : %s", buf));
 			}
 
-			result = avcodec_open2(this->pAudioCtx, audioCodec, NULL);
+			result = avcodec_open2(this->pAudioCtx, audioCodec, nullptr);
 			if (result < 0) {
 				char buf[AV_ERROR_MAX_STRING_SIZE];
 				av_strerror(result, buf, sizeof(buf));
@@ -145,10 +155,10 @@ class AVVideoPlaybackWindow : public VKWindow {
 		AVCodecParameters *pVideoCodecParam = video_st->codecpar;
 
 		AVCodec *pVideoCodec = avcodec_find_decoder(pVideoCodecParam->codec_id);
-		if (pVideoCodec == NULL)
+		if (pVideoCodec == nullptr)
 			throw std::runtime_error("failed to find decoder");
 		this->pVideoCtx = avcodec_alloc_context3(pVideoCodec);
-		if (this->pVideoCtx == NULL)
+		if (this->pVideoCtx == nullptr)
 			throw std::runtime_error("Failed to allocate video decoder context");
 
 		// AV_PIX_FMT_FLAG_RGB
@@ -166,11 +176,14 @@ class AVVideoPlaybackWindow : public VKWindow {
 		// av_find_best_pix_fmt_of_2
 		// avcodec_default_get_format()
 
-		if ((result = avcodec_open2(this->pVideoCtx, pVideoCodec, NULL)) != 0) {
+		if ((result = avcodec_open2(this->pVideoCtx, pVideoCodec, nullptr)) != 0) {
 			char buf[AV_ERROR_MAX_STRING_SIZE];
 			av_strerror(result, buf, sizeof(buf));
 			throw std::runtime_error(fmt::format("Failed to retrieve info from stream info : %s", buf));
 		}
+
+		video_width = this->pVideoCtx->width;
+		video_height = this->pVideoCtx->height;
 
 		this->frame = av_frame_alloc();
 		this->frameoutput = av_frame_alloc();
@@ -180,7 +193,7 @@ class AVVideoPlaybackWindow : public VKWindow {
 		av_image_alloc(this->frameoutput->data, this->frameoutput->linesize, this->pVideoCtx->width,
 					   this->pVideoCtx->height, AV_PIX_FMT_BGRA, 4);
 
-		if (this->frame == NULL)
+		if (this->frame == nullptr)
 			throw std::runtime_error(fmt::format("Failed to allocate frame"));
 
 		// m_bufferSize = avpicture_get_size(PIX_FMT_RGB24, width, height);
@@ -192,7 +205,7 @@ class AVVideoPlaybackWindow : public VKWindow {
 		// AVPacket *pPacket = av_packet_alloc();
 		this->sws_ctx = sws_getContext(this->pVideoCtx->width, this->pVideoCtx->height, this->pVideoCtx->pix_fmt,
 									   this->pVideoCtx->width, this->pVideoCtx->height, AV_PIX_FMT_BGRA, SWS_BICUBIC,
-									   NULL, NULL, NULL);
+									   nullptr, nullptr, nullptr);
 
 		this->frame_timer = av_gettime() / 1000000.0;
 	}
@@ -204,12 +217,12 @@ class AVVideoPlaybackWindow : public VKWindow {
 		/*	*/
 		for (unsigned int i = 0; i < videoImageViews.size(); i++) {
 
-			VKHelper::createImage(getDevice(), width, height, 1, VK_FORMAT_R8G8B8_UNORM, VK_IMAGE_TILING_LINEAR,
+			VKHelper::createImage(getDevice(), video_width, video_height, 1, VK_FORMAT_R8G8B8_UNORM, VK_IMAGE_TILING_LINEAR,
 								  VK_IMAGE_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT,
-								  getLogicalDevice()->getPhysicalDevice(0)->getMemoryProperties(), videoStagingFrames[i],
-								  videoStagingFrameMemory[i]);
+								  getLogicalDevice()->getPhysicalDevice(0)->getMemoryProperties(),
+								  videoStagingFrames[i], videoStagingFrameMemory[i]);
 
-			VKHelper::createImage(getDevice(), width, height, 1, VK_FORMAT_R8G8B8_UNORM, VK_IMAGE_TILING_LINEAR,
+			VKHelper::createImage(getDevice(), video_width, video_height, 1, VK_FORMAT_R8G8B8_UNORM, VK_IMAGE_TILING_LINEAR,
 								  VK_IMAGE_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
 								  getLogicalDevice()->getPhysicalDevice(0)->getMemoryProperties(), videoFrames[i],
 								  videoFrameMemory[i]);
@@ -236,8 +249,8 @@ class AVVideoPlaybackWindow : public VKWindow {
 			imageCopyRegion.extent.height = height;
 			imageCopyRegion.extent.depth = 1;
 
-			vkCmdCopyImage(cmd, videoStagingFrames[0], VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, videoFrames[0],
-						   VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &imageCopyRegion);
+			vkCmdCopyImage(cmd, videoStagingFrames[0], VK_IMAGE_LAYOUT_GENERAL, videoFrames[0],
+						   VK_IMAGE_LAYOUT_GENERAL, 1, &imageCopyRegion);
 
 			VkImageMemoryBarrier imageMemoryBarrier = {};
 			imageMemoryBarrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
@@ -248,10 +261,22 @@ class AVVideoPlaybackWindow : public VKWindow {
 			imageMemoryBarrier.srcAccessMask = VK_ACCESS_MEMORY_WRITE_BIT;
 			imageMemoryBarrier.dstAccessMask = VK_ACCESS_MEMORY_READ_BIT;
 
-			vkCmdPipelineBarrier(cmd, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0,
-								 nullptr, 0, nullptr, 1, &imageMemoryBarrier);
+			vkCmdPipelineBarrier(cmd, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0, nullptr, 0,
+								 nullptr, 1, &imageMemoryBarrier);
+			VkImageBlit blitRegion;
+			blitRegion.srcOffsets[1].x = width;
+			blitRegion.srcOffsets[1].y = height;
+			blitRegion.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			blitRegion.srcSubresource.layerCount = 1;
+			blitRegion.srcSubresource.mipLevel = 0;
+			blitRegion.dstOffsets[1].x = width;
+			blitRegion.dstOffsets[1].y = height;
+			blitRegion.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			blitRegion.dstSubresource.layerCount = 1;
+			blitRegion.dstSubresource.mipLevel = 0;
 
-			//vkCmdBlitImage()
+			vkCmdBlitImage(cmd, videoFrames[0], VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, getSwapChainImages()[i],
+						   VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &blitRegion, VK_FILTER_NEAREST);
 
 			VKS_VALIDATE(vkEndCommandBuffer(cmd));
 		}
@@ -260,82 +285,91 @@ class AVVideoPlaybackWindow : public VKWindow {
 	virtual void draw(void) override {
 		// TODO relocate.
 		// TODO add audio decoder.
-		struct AVPacket packet;
+		AVPacket *packet = av_packet_alloc();
+		if (!packet) {
+			throw std::runtime_error("failed to allocated memory for AVPacket");
+		}
+
 		int res, result;
 		res = av_seek_frame(this->pformatCtx, this->videoStream, 60000, AVSEEK_FLAG_FRAME);
-		while (true) {
+		// while (true) {
 
-			res = av_read_frame(this->pformatCtx, &packet);
-			if (res == 0) {
-				if (packet.stream_index == this->videoStream) {
-					result = avcodec_send_packet(this->pVideoCtx, &packet);
+		res = av_read_frame(this->pformatCtx, packet);
+		if (res == 0) {
+			if (packet->stream_index == this->videoStream) {
+				result = avcodec_send_packet(this->pVideoCtx, packet);
+				if (result < 0) {
+					char buf[AV_ERROR_MAX_STRING_SIZE];
+					av_strerror(result, buf, sizeof(buf));
+					throw std::runtime_error(fmt::format("Failed to send packet for decoding picture frame : %s", buf));
+				}
+
+				while (result >= 0) {
+					result = avcodec_receive_frame(this->pVideoCtx, this->frame);
+					if (result == AVERROR(EAGAIN) || result == AVERROR_EOF)
+						break;
 					if (result < 0) {
 						char buf[AV_ERROR_MAX_STRING_SIZE];
 						av_strerror(result, buf, sizeof(buf));
-						throw std::runtime_error(
-							fmt::format("Failed to send packet for decoding picture frame : %s", buf));
+						throw std::runtime_error(fmt::format(" : %s", buf));
 					}
+					if (this->frame->format != AV_PIX_FMT_BGRA) {
 
-					while (result >= 0) {
-						result = avcodec_receive_frame(this->pVideoCtx, this->frame);
-						if (result == AVERROR(EAGAIN) || result == AVERROR_EOF)
-							break;
-						if (result < 0) {
-							char buf[AV_ERROR_MAX_STRING_SIZE];
-							av_strerror(result, buf, sizeof(buf));
-							throw std::runtime_error(fmt::format(" : %s", buf));
-						}
-						if (this->frame->format != AV_PIX_FMT_BGRA) {
+						if (this->frame->format == AV_PIX_FMT_YUV420P) {
+							this->frame->data[0] =
+								this->frame->data[0] + this->frame->linesize[0] * (this->pVideoCtx->height - 1);
+							this->frame->data[1] =
+								this->frame->data[1] + this->frame->linesize[0] * this->pVideoCtx->height / 4 - 1;
+							this->frame->data[2] =
+								this->frame->data[2] + this->frame->linesize[0] * this->pVideoCtx->height / 4 - 1;
 
-							if (this->frame->format == AV_PIX_FMT_YUV420P) {
-								this->frame->data[0] =
-									this->frame->data[0] + this->frame->linesize[0] * (this->pVideoCtx->height - 1);
-								this->frame->data[1] =
-									this->frame->data[1] + this->frame->linesize[0] * this->pVideoCtx->height / 4 - 1;
-								this->frame->data[2] =
-									this->frame->data[2] + this->frame->linesize[0] * this->pVideoCtx->height / 4 - 1;
+							this->frame->linesize[0] *= -1;
+							this->frame->linesize[1] *= -1;
+							this->frame->linesize[2] *= -1;
+							sws_scale(this->sws_ctx, this->frame->data, this->frame->linesize, 0, this->frame->height,
+									  this->frameoutput->data, this->frameoutput->linesize);
+							/*	Upload the image to staging.	*/
+							void*_data;
+							VKS_VALIDATE(vkMapMemory(getDevice(), videoStagingFrameMemory[0], 0, video_width * video_height * 3, 0, &_data));
+							memcpy(_data, this->frameoutput->data, video_width * video_height * 3);
+							vkUnmapMemory(getDevice(),videoStagingFrameMemory[0]);
+							vkDeviceWaitIdle(getDevice());
 
-								this->frame->linesize[0] *= -1;
-								this->frame->linesize[1] *= -1;
-								this->frame->linesize[2] *= -1;
-								sws_scale(this->sws_ctx, this->frame->data, this->frame->linesize, 0,
-										  this->frame->height, this->frameoutput->data, this->frameoutput->linesize);
-
-								/*	Upload the image.	*/
-							}
 						}
 					}
 				}
-				if (packet.stream_index == this->audioStream) {
-					result = avcodec_send_packet(this->pAudioCtx, &packet);
-					if (result < 0) {
-						char buf[AV_ERROR_MAX_STRING_SIZE];
-						av_strerror(result, buf, sizeof(buf));
-						throw std::runtime_error(
-							fmt::format("Failed to send packet for decoding audio frame : %s", buf));
-					}
-
-					while (result >= 0) {
-						result = avcodec_receive_frame(this->pAudioCtx, this->frame);
-						if (result == AVERROR(EAGAIN) || result == AVERROR_EOF)
-							break;
-						if (result < 0) {
-							char buf[AV_ERROR_MAX_STRING_SIZE];
-							av_strerror(result, buf, sizeof(buf));
-							throw std::runtime_error(fmt::format(" : %s", buf));
-						}
-
-						av_get_channel_layout_nb_channels(this->frame->channel_layout);
-						this->frame->format != AV_SAMPLE_FMT_S16P;
-						this->frame->channel_layout;
-						// clip->setData(this->frame->extended_data[0], this->frame->linesize[0], 0);
-					}
-				}
-
-				printf("duration %f\n", (float)packet.duration);
 			}
-			// av_packet_free(&packet);
+			if (packet->stream_index == this->audioStream) {
+				// result = avcodec_send_packet(this->pAudioCtx, packet);
+				// if (result < 0) {
+				// 	char buf[AV_ERROR_MAX_STRING_SIZE];
+				// 	av_strerror(result, buf, sizeof(buf));
+				// 	throw std::runtime_error(
+				// 		fmt::format("Failed to send packet for decoding audio frame : %s", buf));
+				// }
+
+				// while (result >= 0) {
+				// 	result = avcodec_receive_frame(this->pAudioCtx, this->frame);
+				// 	if (result == AVERROR(EAGAIN) || result == AVERROR_EOF)
+				// 		break;
+				// 	if (result < 0) {
+				// 		char buf[AV_ERROR_MAX_STRING_SIZE];
+				// 		av_strerror(result, buf, sizeof(buf));
+				// 		throw std::runtime_error(fmt::format(" : %s", buf));
+				// 	}
+
+				// 	av_get_channel_layout_nb_channels(this->frame->channel_layout);
+				// 	this->frame->format != AV_SAMPLE_FMT_S16P;
+				// 	this->frame->channel_layout;
+				// 	// clip->setData(this->frame->extended_data[0], this->frame->linesize[0], 0);
+				// }
+			}
+
+			printf("duration %f\n", (float)packet->duration);
 		}
+		av_packet_unref(packet);
+		av_packet_free(&packet);
+		//}
 	}
 
 	virtual void update(void) {}
