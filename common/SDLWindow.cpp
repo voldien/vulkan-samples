341b945d71066df1403ef32e4fd28071ace53c2c
voldie <voldiekami@gmail.com>
2021-07-12 17:09:30 +0200

    Code formated

--- common/SDLWindow.cpp
+++ common/SDLWindow.cpp
@@ -1,17 +1,17 @@
-#include"SDLWindow.h"
+#include "SDLWindow.h"
 
-void SDLWindow::show()noexcept { SDL_ShowWindow(this->window); }
+void SDLWindow::show() noexcept { SDL_ShowWindow(this->window); }
 
-void SDLWindow::hide()noexcept { SDL_HideWindow(this->window); }
+void SDLWindow::hide() noexcept { SDL_HideWindow(this->window); }
 
 void SDLWindow::close(void) noexcept {
 	this->hide();
 	SDL_DestroyWindow(this->window);
 }
 
-void SDLWindow::setPosition(int x, int y)  noexcept{ SDL_SetWindowPosition(this->window, x, y); }
+void SDLWindow::setPosition(int x, int y) noexcept { SDL_SetWindowPosition(this->window, x, y); }
 
-void SDLWindow::setSize(int width, int height)noexcept {
+void SDLWindow::setSize(int width, int height) noexcept {
 	/*	TODO determine if it shall update framebuffera as well.	*/
 	SDL_SetWindowSize(this->window, width, height);
 }
@@ -23,17 +23,17 @@ void SDLWindow::setTitle(const std::string &title) { SDL_SetWindowTitle(window, 
 
 std::string SDLWindow::getTitle(void) { return SDL_GetWindowTitle(window); }
 
-int SDLWindow::x(void) const noexcept{
+int SDLWindow::x(void) const noexcept {
 	int x, y;
 	SDL_GetWindowPosition(this->window, &x, &y);
 	return x;
 }
-int SDLWindow::y(void) const noexcept{
+int SDLWindow::y(void) const noexcept {
 	int x, y;
 	SDL_GetWindowPosition(this->window, &x, &y);
 	return y;
 }
-void SDLWindow::resizable(bool resizable) noexcept{ SDL_SetWindowResizable(this->window, (SDL_bool)resizable); }
+void SDLWindow::resizable(bool resizable) noexcept { SDL_SetWindowResizable(this->window, (SDL_bool)resizable); }
 
 void SDLWindow::setFullScreen(bool fullscreen) {
 	// TODO add option for using either of the modes.
@@ -47,27 +47,22 @@ bool SDLWindow::isFullScreen(void) const { return false; }
 
 void SDLWindow::setBordered(bool bordered) { SDL_SetWindowBordered(this->window, (SDL_bool)bordered); }
 
-int SDLWindow::width(void) const noexcept{
+int SDLWindow::width(void) const noexcept {
 	int w, h;
 	getSize(&w, &h);
 	return w;
 }
-int SDLWindow::height(void) const noexcept{
+int SDLWindow::height(void) const noexcept {
 	int w, h;
 	getSize(&w, &h);
 	return h;
 }
 
 void SDLWindow::setMinimumSize(int width, int height) { SDL_SetWindowMinimumSize(this->window, width, height); }
-void SDLWindow::getMinimumSize(int *width, int *height) {
-	SDL_GetWindowMinimumSize(this->window, width, height);
-
-}
+void SDLWindow::getMinimumSize(int *width, int *height) { SDL_GetWindowMinimumSize(this->window, width, height); }
 
 void SDLWindow::setMaximumSize(int width, int height) { SDL_SetWindowMaximumSize(this->window, width, height); }
-void SDLWindow::getMaximumSize(int *width, int *height) {
-	SDL_GetWindowMaximumSize(this->window, width, height);
-}
+void SDLWindow::getMaximumSize(int *width, int *height) { SDL_GetWindowMaximumSize(this->window, width, height); }
 
 void SDLWindow::focus(void) { SDL_SetWindowInputFocus(this->window); }
 
